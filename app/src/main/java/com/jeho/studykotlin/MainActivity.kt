167b8b561494df180a41b881e6aa6b180668ddb4
Jeho Yeon <jehoyeon617@gmail.com>
2021-10-08 23:09:12 +0900

    #9 Essential_Android

--- app/src/main/java/com/jeho/studykotlin/MainActivity.kt
+++ app/src/main/java/com/jeho/studykotlin/MainActivity.kt
@@ -2,10 +2,39 @@ package com.jeho.studykotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.final_practice)
+
+        Log.d("life_cycle", "onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("life_cycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life_cycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("life_cycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("life_cycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("life_cycle", "onDestroy")
+    }
+
 }
